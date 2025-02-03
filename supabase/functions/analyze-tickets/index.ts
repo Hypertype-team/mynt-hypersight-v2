b08991998ee6e73e61e859970123161f95acd2f8
Manuel Fraile <manuelfraile@Manuels-MacBook-Pro.local>
2025-02-03 15:13:28 +0100

    Remove google auth import

--- supabase/functions/analyze-tickets/index.ts
+++ supabase/functions/analyze-tickets/index.ts
@@ -1,6 +1,5 @@
 import "https://deno.land/x/xhr@0.1.0/mod.ts";
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-import { GoogleAuth } from 'google-auth-library';
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
